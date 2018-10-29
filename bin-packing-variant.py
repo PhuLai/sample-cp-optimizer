9e7f95bfa030081a771e0d5cdbab670df8c8c7bd
Phu Lai <thanhphu410@gmail.com>
2018-10-29 13:27:36 +1100

    Minor changes

--- bin-packing-variant.py
+++ bin-packing-variant.py
@@ -1,9 +1,6 @@
 """
 This example solves a variant of the bin packing problem using IBM CP Optimizer
 
-Similar problem as bin-packing-variant.py but with new built-in constraint
-http://ibmdecisionoptimization.github.io/docplex-doc/cp/docplex.cp.modeler.py.html#docplex.cp.modeler.pack
-
 Given:
     - A set of m identical items. Item size is 4-dimensional
     - A set of n bins with different size. Bin size is 4-dimensional
@@ -18,7 +15,6 @@ Objective:
 """
 from docplex.cp.model import CpoModel
 import numpy as np
-import math
     
 class Item(object):
     def __init__(self, id, size):
@@ -31,9 +27,9 @@ class Bin(object):
         self.size = size
         
 #===============IMPORT DATA===============
-items_data = np.loadtxt('set512-items.txt', dtype = np.integer, delimiter=',')
-bins_data = np.loadtxt('set512-bins.txt', dtype = np.integer, delimiter=',')
-alloc_constraint = np.loadtxt('set512-alloc-constraint.txt', dtype = np.integer, delimiter=',')
+items_data = np.loadtxt('set-items.txt', dtype = np.integer, delimiter=',')
+bins_data = np.loadtxt('set-bins.txt', dtype = np.integer, delimiter=',')
+alloc_constraint = np.loadtxt('set-alloc-constraint.txt', dtype = np.integer, delimiter=',')
 
 nb_bins = len(bins_data)
 nb_items = len(items_data)
@@ -49,6 +45,7 @@ for i in range(len(bins_data)):
     bins.append(Bin(i, bins_data[i]))
     
 #add a new bin at the end of the list, big enough to accommodate ALL items
+#needed so that all items can be allocated, CPO won't return any solution if an item unallocated    
 bins.append(Bin(nb_bins, sum(items_data)))
 
 #===============CREATE MODEL===============
@@ -76,27 +73,28 @@ for k in range(4):
     
 #===============SETUP OBJECTIVE=============== 
 #maximize number of items allocated to all bins EXCEPT the big bin
-nb_allocated_items = mdl.sum([(wheres[int(item.id)] != nb_bins) for item in items[0:nb_items]])
+nb_allocated_items = mdl.sum([(wheres[int(item.id)] != nb_bins) for item in items])
 
 #maximize x_1 * x_2 * ... *x_n, where x_1, x_2,..., x_n is the number of items in bin 1, 2, ..., n
 product_x = 1
 for bin in bins[0:len(bins)-1]:
+    #get the number of items in the bin
     nb_items_in_bin = mdl.sum([(wheres[int(item.id)] == int(bin.id)) for item in items])
+    #if the bin is empty, assume it has 1 item so that the product won't be zero
     x = mdl.conditional(nb_items_in_bin != 0, nb_items_in_bin, 1)
-    x = x/100
-    product_x = mdl.times(product_x, x)
+    #divided by 10 so that CPO won't have to handle big integer
+    product_x = mdl.times(product_x, x/10)
         
+#add lecicographic objs
 mdl.add(mdl.maximize_static_lex([nb_allocated_items, product_x]))
 
 #solve the problem and print the solution
 print("...solving...")
 msol = mdl.solve(url = None, key = None, TimeLimit = None, SearchType = 'Auto')
 msol.print_solution()
-print("Obj bounds: " + str(msol.get_objective_bounds()))
-print("Obj gaps: " + str(msol.get_objective_gaps()))
 print("Obj vals: " + str(msol.get_objective_values()))
-print("prodyuct",msol.get_objective_values()[1]*(100**len(bins)-1))
-print("CPU: " + str(-math.log(msol.get_objective_values()[1]*(100**len(bins)-1))/math.log(0.93)))
+print("x_1 * x_2 * ... * x_n = ",msol.get_objective_values()[1]*(10**nb_bins))
 mdl.export_as_cpo(out='cpo.txt')
 with open('log.txt', "w") as text_file:
     print(msol.get_solver_log(), file=text_file)
+    
